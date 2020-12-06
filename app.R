fd52188739b6b16301370cb0f6f69d587bc3ca04
Diana Lin <dlin@bcgsc.ca>
2020-12-05 17:05:07 -0800

    working version of sidebar layout

---
+++ app.R
@@ -0,0 +1,82 @@
+library(shiny)
+library(here)
+library(tidyverse)
+library(lubridate)
+library(DT)
+library(glue)
+
+options(shiny.autoreload = TRUE)
+
+# Read in the TSV file
+data <-
+    read_tsv(
+        here("data", "COVID Takeout - Sheet1.tsv"),
+        col_types = cols (
+            Date = col_character(),
+            Restaurant = col_character(),
+            App = col_character(),
+            Item = col_character(),
+            Cuisine = col_character(),
+            Type = col_character()
+        )
+    ) %>%
+    mutate(Date = dmy(Date))
+
+# change layout to do rows
+ui <- fluidPage(# Application title
+    titlePanel("COVID-19 Pandemic Lockdown Takeout"),
+    
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(sidebarPanel(
+        selectInput(
+            "select",
+            label = p("Select y-axis for bar plot:"),
+            choices = colnames(data)[-1],
+            selected = "Restaurant"
+        ),
+        dateRangeInput("dates", label = p("Filter table by date range:"), start = "2020-03-12", end = "2020-12-31")),
+        mainPanel(
+            DT::dataTableOutput("table"),
+            plotOutput("plot"),
+            plotOutput("timeplot"))
+    )
+)
+
+# Define server logic
+server <- function(input, output) {
+    output$table <- renderDataTable({
+        arrange(data, Date) %>%
+            filter(Date >= input$dates[1],
+                   Date <= input$dates[2])
+    })
+    output$plot <- renderPlot({
+        data %>%
+            mutate(fct = factor(!!sym(input$select)
+            ) %>%
+            fct_infreq() %>%
+            fct_rev()) %>%
+            drop_na(!!sym(input$select)) %>%
+            ggplot(aes(fct)) +
+            geom_bar() +
+            coord_flip() +
+            labs(x = input$select,
+                 title = glue("Breakdown by {input$select}")) +
+            theme_minimal() +
+            theme(legend.position = "none")
+    })
+    output$timeplot <- renderPlot({
+        data %>%
+            mutate(month = month(Date, label = TRUE)) %>%
+            filter(Date >= input$dates[1],
+                   Date <= input$dates[2]) %>%
+            arrange(month) %>%
+            count(month) %>% 
+            ggplot(aes(month, n, group = 1)) +
+            geom_line() +
+            geom_point() +
+            labs(x = "Month", y = "count")
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
